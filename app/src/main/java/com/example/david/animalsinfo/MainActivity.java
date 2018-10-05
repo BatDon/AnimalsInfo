6a921c335a6dbf49135c7c7b60b9d826981dce4e
BatDon <asics007@gmail.com>
2018-10-04 19:49:12 -0500

    All three buttons work return correct values.

--- app/src/main/java/com/example/david/animalsinfo/MainActivity.java
+++ app/src/main/java/com/example/david/animalsinfo/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int MAIN_THREAD_TASK_1 = 1;
     private int MAIN_THREAD_TASK_2 = 2;
-    private int CHILD_THREAD_QUIT_LOOPER = 3;
+    private int MAIN_THREAD_TASK_3 = 3;
+    private int CHILD_THREAD_QUIT_LOOPER = 4;
 
     private Handler mainThreadHandler;
 
@@ -39,10 +40,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton runTaskTwoButton;
 
+    private ImageButton runTaskThreeButton;
     public TextView taskStatusTextView;
 
     public String dogTextHtml;
     public String dogText;
+
+    public String catTextHtml;
+    public String catText;
+
+    public String frogTextHtml;
+    public String frogText;
 
     public Reader reader;
 
@@ -67,10 +75,10 @@ public class MainActivity extends AppCompatActivity {
                     taskStatusTextView.setText(dogText);}
                     else if (msg.what == MAIN_THREAD_TASK_2) {
                         // If task two button is clicked.
-                        taskStatusTextView.setText("Task two execute.");}
-                        else if (msg.what == CHILD_THREAD_QUIT_LOOPER) {
+                        taskStatusTextView.setText(catText);}
+                        else if (msg.what == MAIN_THREAD_TASK_3) {
                         // If quit child thread looper button is clicked.
-                        taskStatusTextView.setText("Quit child thread looper.");
+                        taskStatusTextView.setText(frogText);
                     }
                 }
             }
@@ -81,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
             runTaskOneButton =findViewById(R.id.dogButton);
 
             runTaskTwoButton =findViewById(R.id.catButton);
+
+            runTaskThreeButton=findViewById(R.id.frogButton);
 
             // Set on click listener to each button.
         runTaskOneButton.setOnClickListener(new View.OnClickListener()
@@ -107,22 +117,32 @@ public class MainActivity extends AppCompatActivity {
                 workerThread.workerThreadHandler.sendMessage(msg);
             }
             });
+        runTaskThreeButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View view){
+                Message msg = new Message();
+                msg.what = MAIN_THREAD_TASK_3;
+                workerThread.workerThreadHandler.sendMessage(msg);
+            }
+        });
 
 
             // Get status info TextView object.
             taskStatusTextView =findViewById(R.id.somethingText);
 
             // Get the quit child thread looper button.
-            ImageButton quitChildThreadLooperButton = findViewById(R.id.frogButton);
-        quitChildThreadLooperButton.setOnClickListener(new View.OnClickListener()
+           /* ImageButton quitChildThreadLooperButton = findViewById(R.id.frogButton);
+        quitChildThreadLooperButton.setOnClickListener(new View.OnClickListener()*/
 
-            {
+            /*{
                 @Override
                 public void onClick (View view){
                 // Click this button will quit child thread looper.
                 workerThread.workerThreadHandler.getLooper().quit();
             }
-            });
+            });*/
         }
 
         // This child thread class has it's own Looper and Handler object.
@@ -138,6 +158,13 @@ public class MainActivity extends AppCompatActivity {
                 Looper.prepare();
                 FormatDocument formattedDoc=new FormatDocument();
                 dogText=formattedDoc.returnString();
+
+                FormatDocument2 formattedDoc2=new FormatDocument2();
+                catText=formattedDoc2.returnString2();
+
+                FormatDocument3 formattedDoc3=new FormatDocument3();
+                frogText=formattedDoc3.returnString3();
+
 
                 //final StringBuilder builder = new StringBuilder();
                 //String dogText = formatted.returnString();
@@ -272,4 +299,102 @@ public class MainActivity extends AppCompatActivity {
                 return dogText;
             }
         }
+    public class FormatDocument2 {
+        public String returnString2() {
+
+
+            try {
+                String url = "https://mydavidjerome.com/android-app/";
+                Document doc = Jsoup.connect(url).get();
+                String title = doc.title();
+
+
+                Element titleElement;
+                Element parElement;
+                String combinedString;
+
+
+                String dogTitle;
+                String dogsPar = null;
+                Element e2 = doc.select("p:contains(Cats)").get(0);
+                //Still hast HTML tags
+                dogTitle = e2.toString();
+                String parString = e2.nextElementSibling().toString();
+                String dogTextHtml = dogTitle + parString;
+
+                Document doc2 = Jsoup.parse(dogTextHtml);
+
+
+                doc2.outputSettings(new Document.OutputSettings().prettyPrint(false));//makes html() preserve linebreaks and spacing
+                doc2.select("p").append("\\n");
+                doc2.select("li").prepend("\\n\\n");
+                String s = doc2.html().replaceAll("\\\\n", "\n");
+                catText = Jsoup.clean(s, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
+
+                //combinedString=e2+parElement;
+
+                Log.i("in Element", "paragraph of dogs");
+
+
+                Log.i("JSoup", "Connected successfully!");
+
+            }
+
+            catch (IOException e) {
+                final StringBuilder builder = new StringBuilder();
+                builder.append("Error : ").append(e.getMessage()).append("\n");
+                Log.i("HTML ERROR", "exception reading HTML");
+            }
+            return catText;
+        }
+    }
+    public class FormatDocument3 {
+        public String returnString3() {
+
+
+            try {
+                String url = "https://mydavidjerome.com/android-app/";
+                Document doc = Jsoup.connect(url).get();
+                String title = doc.title();
+
+
+                Element titleElement;
+                Element parElement;
+                String combinedString;
+
+
+                String dogTitle;
+                String dogsPar = null;
+                Element e2 = doc.select("p:contains(Frogs)").get(0);
+                //Still hast HTML tags
+                dogTitle = e2.toString();
+                String parString = e2.nextElementSibling().toString();
+                String dogTextHtml = dogTitle + parString;
+
+                Document doc2 = Jsoup.parse(dogTextHtml);
+
+
+                doc2.outputSettings(new Document.OutputSettings().prettyPrint(false));//makes html() preserve linebreaks and spacing
+                doc2.select("p").append("\\n");
+                doc2.select("li").prepend("\\n\\n");
+                String s = doc2.html().replaceAll("\\\\n", "\n");
+                frogText = Jsoup.clean(s, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
+
+                //combinedString=e2+parElement;
+
+                Log.i("in Element", "paragraph of dogs");
+
+
+                Log.i("JSoup", "Connected successfully!");
+
+            }
+
+            catch (IOException e) {
+                final StringBuilder builder = new StringBuilder();
+                builder.append("Error : ").append(e.getMessage()).append("\n");
+                Log.i("HTML ERROR", "exception reading HTML");
+            }
+            return frogText;
+        }
+    }
     }
